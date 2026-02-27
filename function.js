7cd2f893c7b847fa2c4b9cf07fa519f73d26ad43
natalybarillas9-gif <natalybarillas9@gmail.com>
2026-02-27 07:50:33 -0600

    Add files via upload

--- function.js
+++ function.js
@@ -1,19 +1,39 @@
 //ejemplo 
 const nameInput = document.getElementById('nameInput');
 const colorPicker = document.getElementById('colorPicker');
-const displayName = document.getElementById('displayName');
 const banner = document.querySelector('.banner');
+const displayName = document.getElementById('displayName');
+const jobInput=document.getElementById('jobInput')
+const displayjob=document.querySelector('.card p');
+const fontPicker=document.getElementById('fontPicker');
+const card= document.getElementById('profileCard');
+const darkModeBtn=document.getElementById('darkModeBtn');
+
 
 
 
 //actualizar el nombre a ingresar 
-nameInput.addEventListener('input',(e) =>{
+nameInput.addEventListener('input',(e) => {
 	displayName.textContent = e.target.value || "escribe tu nombre aqui"   
+});
+
+//actualizar el cargo a ingresar
+jobInput.addEventListener('input',(e) => {
+	displayjob.textContent =e.target.value || "Desarrollador Wed"
 });
 
 //actualizar color de fondo 
 
-colorPicker.addEventListener('input',(e) =>{
-	banner.style.backgroundColor = e.target.value;
+colorPicker.addEventListener('input', (e) => {
+	banner.style.background = e.target.value;
 });
 
+//actualizar el tipo de letra
+fontPicker.addEventListener('change',(e)=>{
+	card.style.fontFamily=e.target.value;
+});
+
+//Modo oscuro
+darkModeBtn.addEventListener('click',()=>{
+	card.classList.toggle('dark-mode');
+});
